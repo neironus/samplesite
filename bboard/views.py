075e1b1e3f9e88df83e7e5621df957d976822e52
Maksym Dakal <dakalm@gmail.com>
2019-06-19 16:55:10 +0300

    forms dronov page 65

--- bboard/views.py
+++ bboard/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Bb, Rubric
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .forms import BbForm
 
 # Create your views here.
 
@@ -18,3 +21,13 @@ def by_rubric(requset, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs':bbs, 'rubrics':rubrics, 'current_rubric':current_rubric}
     return render(requset, 'bboard/by_rubric.html', context)
+
+class BdCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
